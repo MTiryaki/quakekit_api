5be982fad98048c8514d2a92d703e2c29590a9d4
Hasan Batuhan Kurt <hasanbatuhankurt98@gmail.com>
2021-01-10 23:42:13 +0300

    Last Update

--- API.QUAKEKIT/Models/MeetingArea.cs
+++ API.QUAKEKIT/Models/MeetingArea.cs
@@ -8,8 +8,12 @@ namespace API.QUAKEKIT.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string maID { get; set; }
+        public string mID { get; set; }
         public string maTitle { get; set; }
-        public int maCityID { get; set; }
-        public int maDistrictID { get; set; }
+        public float maLat { get; set; }
+        public float maLng { get; set; }
+        public string maCity { get; set; }
+        public string maDistrict { get; set; }
+        public string maNeighborhood { get; set; }
     }
 }
